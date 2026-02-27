c2123678b86028608b49adbdcb8cfdaec7bbc81f
Eduarda Ferreira Costa <77804572+dudsfcosta@users.noreply.github.com>
2026-02-26 23:05:16 -0300

    API integration pt2

--- app/filmes/em-alta/page.tsx
+++ app/filmes/em-alta/page.tsx
@@ -2,6 +2,8 @@ import Title from "@/app/components/Title"
 import {getNowPlaying} from "@/lib/api/tmdb";
 import Grid from "@/app/components/Grid";
 
+export const dynamic = 'force-dynamic';
+
 const FilmesEmAlta = async () => {
 
     const filmes = await getNowPlaying();
--- app/filmes/populares/page.tsx
+++ app/filmes/populares/page.tsx
@@ -2,6 +2,8 @@ import Title from "@/app/components/Title"
 import {getPopularMovies} from "@/lib/api/tmdb";
 import Grid from "@/app/components/Grid";
 
+export const revalidate = 60;
+
 const FilmesPopulares = async () => {
 
     const filmes = await getPopularMovies();
